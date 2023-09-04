1a0c65f59fefc236243731583fc1bfc5cfaa4391
nayeongdev <nayeongkim.dev@gmail.com>
2023-09-04 17:44:47 +0900

    객체를 이용하여 유저의 입력값을 저장

--- js/app.js
+++ js/app.js
@@ -18,7 +18,9 @@ $('.menu-close-btn').addEventListener('click', () => {
 });
 
 function App() {
-  let data = [];
+  const data = [];
+  const userData = {};
+  const groupData = {};
 
   data.push({
     "role": "system",
@@ -44,48 +46,56 @@ function App() {
       });
   }
 
-  // form태그가 자동으로 전송되는걸 막아준다. (ENTER키를 눌렀을 때 새로고침 되는걸 막아준다.)
-  $('#peronal-info-submit-btn').addEventListener('click', e => {
+  $('#peronal-info-btn').addEventListener('click', e => {
     e.preventDefault();
-    console.log("실행중");
     const goal = $('#user-goal');
     const age = $('#user-age');
     const gender = $('input[name ="user-gender"]:checked');
     const height = $('#user-height');
     const weight = $('#user-weight');
+
+    userData = {
+      goal: goal.value,
+      age: age.value + "살",
+      gender: gender.value,
+      height: height.value + "cm",
+      weight: weight.value + "kg",
+    }
+  });
+
+  // form태그가 자동으로 전송되는걸 막아준다. (ENTER키를 눌렀을 때 새로고침 되는걸 막아준다.)
+  $('#peronal-info-submit-btn').addEventListener('click', e => {
+    e.preventDefault();
+
+    const level = $('#user-level');
     const place = $('#user-place');
     const preferredActivity = $('input[name ="user-preferred-activity"]:checked');
     const focusOnPart = $('#focus-on-body-part');
-    const level = $('#user-level');
     const exerciseTime = $('#user-exercise-time');
+
+    userData.level = level.value;
+    userData.place = place.value;
+    userData.preferredActivity = preferredActivity.value;
+    userData.focusOnPart = focusOnPart.value;
+    userData.exerciseTime = exerciseTime.value + "분";
+
+    // console.log(userData);
+
+    let userDataContent = ''
+    for (let key in userData) {
+      if (userData.hasOwnProperty(key)) {
+        userDataContent += `${key} : ${userData[key]}\n`;
+      }
+    }
+    // console.log(userDataContent);
 
     data.push({
       "role": "user",
       "content": `
       아래 정보로 개인 맞춤형 운동 루틴을 계획해줘.
-      ${goal.innerText} : ${goal.value}
-      ${gender.innerText} : ${gender.value}
-      ${age.innerText} : ${age.value}살
-      ${height.innerText} : ${height.value}cm
-      ${weight.innerText} : ${weight.value}kg
-      ${level.innerText} : ${level.value}
-      ${place.innerText} : ${place.value}
-      ${preferredActivity.innerText} : ${preferredActivity.value}
-      ${focusOnPart.innerText} : ${focusOnPart.value}
-      ${exerciseTime.innerText} : ${exerciseTime.value}
+      ${userDataContent}
       `
     });
-
-    goal.value = "";
-    age.value = "";
-    gender.value = "";
-    height.value = "";
-    weight.value = "";
-    level.value = "";
-    place.value = "";
-    preferredActivity.value = "";
-    focusOnPart.value = "";
-    exerciseTime.value = "";
 
     OpenApi();
   });
@@ -93,7 +103,7 @@ function App() {
 
   $('#group-info-submit-btn').addEventListener('click', e => {
     e.preventDefault();
-    console.log("실행중");
+
     const participantsNum = $('#participants-num');
     const gender = $('input[name ="group-gender"]:checked');
     const goal = $('#group-goal');
@@ -102,27 +112,29 @@ function App() {
     const preferredActivity = $('#group-preferred-activity');
     const exerciseTime = $('#group-exercise-time');
 
+    groupData.participantsNum = participantsNum.value + "명";
+    groupData.gender = gender.value;
+    groupData.goal = goal.value;
+    groupData.level = level.value;
+    groupData.place = place.value;
+    groupData.preferredActivity = preferredActivity.value;
+    groupData.exerciseTime = exerciseTime.value + "분";
+
+    let groupDataContent = ''
+    for (let key in groupData) {
+      if (groupData.hasOwnProperty(key)) {
+        groupDataContent += `${key} : ${groupData[key]}\n`;
+      }
+    }
+    console.log(groupDataContent);
+
     data.push({
       "role": "user",
       "content": `
-      아래 정보로 그룹 맞춤형 운동 루틴을 계획해줘.
-      ${participantsNum.innerText} : ${participantsNum.value}명
-      ${gender.innerText} : ${gender.value}
-      ${goal.innerText} : ${goal.value}
-      ${level.innerText} : ${level.value}
-      ${place.innerText} : ${place.value}
-      ${preferredActivity.innerText} : ${preferredActivity.value}
-      ${exerciseTime.innerText} : ${exerciseTime.value}
+      아래 정보로 개인 맞춤형 운동 루틴을 계획해줘.
+      ${groupDataContent}
       `
     });
-
-    participantsNum.value = "";
-    gender.value = "";
-    goal.value = "";
-    level.value = "";
-    place.value = "";
-    preferredActivity.value = "";
-    exerciseTime.value = "";
 
     OpenApi();
   });
